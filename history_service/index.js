43aa872678e1ec20103e4395ab25995f4ec8715a
HoleHorse <tamirlankulzhanov@gmail.com>
2023-10-20 04:38:33 +0600

    history pagination

--- history_service/index.js
+++ history_service/index.js
@@ -36,7 +36,6 @@ async function connectQueue() {
 }
 
 emitter.on("action", async (action) => {
-  console.log("action => " + `${action}`);
   const history = await prisma.history.create({
     data: { ...action.data },
   });
@@ -49,4 +48,41 @@ app.listen(PORT, (error) => {
     return;
   }
   console.log(`History service listening on http://localhost:${PORT}`);
+});
+
+app.get("/history/page/:page", async (req, res) => {
+  try {
+    const page = parseInt(req.params.page);
+    if (!Number.isInteger(page) || page <= 0) {
+      return res.status(400).json({ message: "Invalid page parameter" });
+    }
+
+    let limit = parseInt(req.query.limit);
+    if (!limit || limit <= 0 || !Number.isInteger(limit)) {
+      return res.status(400).json({ message: "Invalid limit parameter" });
+    }
+
+    const count = await prisma.history.count();
+    const maxPage = Math.ceil(count / limit);
+    if (page > maxPage) {
+      return res.status(400).json({ message: "Page out of bounds" });
+    }
+
+    const history_chunk = await prisma.history.findMany({
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+    const result = {
+      data: history_chunk,
+      maxPage: maxPage,
+      currentPage: page,
+      nextPage: page < maxPage ? page + 1 : null,
+      prevPage: page > 1 ? page - 1 : null,
+    };
+    res.status(200).json(result);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error: " + error.message });
+  }
 });
